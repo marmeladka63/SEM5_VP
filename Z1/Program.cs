6c35ace9804b1634fa99489e6de64ea28a020cb7
marmelaka63 <peshkova-va@yandex.ru>
2022-04-28 16:19:06 +0300

    Семирнар 4

---
+++ Z1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Создать массив 12 элементов, заполненых в случайном порядке от -9 до +9 и посчитать отдельно отрицательные, отдельно положительные.
+
+using System;
+
+Console.Clear();
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+int pozitivSum = 0;
+int negativSum = 0;
+
+// чтение элементов массива
+foreach (int el in array)
+{
+    pozitivSum += el > 0 ? el : 0;
+    negativSum += el < 0 ? el : 0;
+}
+Console.WriteLine($"Pozitiv Sum= {pozitivSum}, Negativ Sum = {negativSum}");
+
+// Метод получения массива
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
---
+++ Z2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа замены элементов массива - положительные на отрицательные, отрицательные на положительные.
+
+Console.Clear();
+
+int[] array = GetArray(12, -9, 9);
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
