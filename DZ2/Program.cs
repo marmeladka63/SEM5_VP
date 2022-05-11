182443c9995cdf78e69163ce778f8cc61b30bd1d
marmeladka63 <peshkova-va@yandex.ru>
2022-05-11 23:55:02 +0300

    Домашнее задание 2

---
+++ DZ2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+using System.Linq;
+Console.Clear();
+int[] array= new int[4].Select(x=>new Random().Next(-100,100)).ToArray();
+Console.WriteLine(String.Join(" ", array));
+
+Odd(array);
+void Odd(int[] array)
+{
+   int sum=0;
+for(int i=0; i<4; i++)
+{
+    if(i%2==1)
+    sum=sum+array[i];
+}
+    
+    Console.WriteLine(sum);
+}
