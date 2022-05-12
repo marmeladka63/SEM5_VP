590f84cb4775cc161904a2df7cc9baa33f52b90a
marmelaka63 <peshkova-va@yandex.ru>
2022-05-12 15:13:00 +0300

    Дорешали дз3

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -2,28 +2,51 @@
 
 //[3 7 22 2 78] -> 76
 
+using System;
+
 using System.Linq;
-//Console.Clear();
-double[] array= new double[5];
+
+Console.Clear();
+double[] array = GetArray(5, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+/*Console.WriteLine(String.Join(" ", array));
+
+double [] arr (double[] array)
+{
+
+double[] arr= new double[5];
 
 
 Random rand = new Random();
 for (int i = 0; i < 5; i++)
 {
-array[i] =rand.Next(-100, 100)+ rand.NextDouble();
+arr[i] =rand.Next(-100, 100)+ rand.NextDouble();
 }
+return array;
+} */
 
-Console.WriteLine(String.Join(" ", array));
 
-double max=0;
-for (int i = 0; i < 5; i++)
+
+double max = 0;
+for (int j = 0; j <= 5; j++)
 {
-
-
-if (array[i]>array[i+1])
-{
-    max=array[i];
+    if (array[j] > array[j + 1])
+    {
+        max = array[j];
+    }
+    else j++;
 }
-else i++;
-}
+
 Console.WriteLine(max);
+
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
---
+++ DZ33/Program.cs
@@ -0,0 +1,47 @@
+﻿//  Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+using System;
+
+using System.Linq;
+
+Console.Clear();
+double[] array = GetArray(5, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+
+double min = array[0];
+for (int j = 0; j < 5; j++)
+{
+    if (min > array[j])
+    {
+        min = array[j];
+
+    }
+}
+
+
+double max = array[0];
+for (int j = 0; j < 5; j++)
+{
+    if (max < array[j])
+    {
+        max = array[j];
+
+    }
+}
+Console.WriteLine($"sum = {max + min}");
+
+
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
